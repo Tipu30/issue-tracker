10aebd6908fc08398f2963dd71198ebf4904028f
Tipu <tipukhan2315@gmail.com>
2024-07-14 11:57:46 +0530

    last commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-import Image from 'next/image'
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
+import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
+import { Flex, Grid } from '@radix-ui/themes';
+import { Metadata } from 'next';
 
-export default function Home() {
+export default async function Home() {
+  //   const open = await prisma.issue.count({
+  //     where: { status: 'OPEN' },
+  //   });
+  //   const inProgress = await prisma.issue.count({
+  //     where: { status: 'IN_PROGRESS' },
+  //   });
+  //   const closed = await prisma.issue.count({
+  //     where: { status: 'CLOSED' },
+  //   });
+
   return (
-    <div>
-      <h1>
-        Hello from tipu
-      </h1>
-    </div>
-  )
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Flex direction="column" gap="5">
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'Issue Tracker - Dashboard',
+  description: 'View a summary of project issues'
+};
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3b4039e8d7462833c38b10296275114e@o4507597987905536.ingest.de.sentry.io/4507598002192464",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
